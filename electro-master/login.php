fc9dc9befbbfb29b11ad0e6bae8eaf9aedae56a7
Student <Student@tdc.edu.vn>
2022-11-15 09:37:52 +0700

    newcode

--- electro-master/login.php
+++ electro-master/login.php
@@ -1,23 +1,22 @@
 <?php 
 include "header.php"
 ?>
-
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <link rel="stylesheet" href=""/>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>login</title>
 </head>
-
 <body>
-    <form action='login.php' class="dangnhap" method='POST'>
+<form action='login.php' class="dangnhap" method='POST'>
         Tên đăng nhập : <input type='text' name='username' />
         Mật khẩu : <input type='password' name='password' />
         <input type='submit' class="button" name="dangnhap" value='Đăng nhập' />
-        <a href='dangky.php' title='Đăng ký'>Đăng ký</a>
+        <a href='Resgister.php' title='Đăng ký'>Đăng ký</a>
         <?php require 'xuly.php'; ?>
         <form>
 </body>
-
 </html>
